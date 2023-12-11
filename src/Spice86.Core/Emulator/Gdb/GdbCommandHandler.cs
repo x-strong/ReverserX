665a76cc710d0386ac768121aafb1b5990111e2e
Maximilien Noal <noal.maximilien@gmail.com>
2023-12-11 18:15:18 +0100

    Gdb: typo
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Gdb/GdbCommandHandler.cs
+++ src/Spice86.Core/Emulator/Gdb/GdbCommandHandler.cs
@@ -93,7 +93,7 @@ public class GdbCommandHandler {
                 'P' => _gdbCommandRegisterHandler.WriteRegister(commandContent),
                 'm' => _gdbCommandMemoryHandler.ReadMemory(commandContent),
                 'M' => _gdbCommandMemoryHandler.WriteMemory(commandContent),
-                'T' => HandleThreadALive(),
+                'T' => HandleThreadAlive(),
                 'v' => ProcessVPacket(commandContent),
                 's' => _gdbCommandBreakpointHandler.Step(),
                 'z' => _gdbCommandBreakpointHandler.RemoveBreakpoint(commandContent),
@@ -119,7 +119,7 @@ public class GdbCommandHandler {
         return _gdbIo.GenerateResponse("");
     }
 
-    private string HandleThreadALive() {
+    private string HandleThreadAlive() {
         return _gdbIo.GenerateResponse("OK");
     }
 
